bd30c8b2fb4f2b92b41f7c8674107a0d05c680b6
Adrian Kryzinski <adriankryzinski@gmail.com>
2020-03-11 18:38:35 +0100

    Add booking class

---
+++ src/js/components/Booking.js
@@ -0,0 +1,22 @@
+/* eslint-disable linebreak-style */
+
+class Booking{
+  constructor(bookingWidget){
+    const thisBooking = this;
+  
+    thisBooking.render(bookingWidget);
+    thisBooking.initWidgets();
+  }
+  
+  render(bookingWidget){
+    const thisBooking = this;
+  }
+
+  initWidgets(){
+    const thisBooking = this;
+  }
+
+
+}
+
+export default Booking;
