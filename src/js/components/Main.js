180925a7548373eeb22cf1ff02aae7dc13dae425
Adrian Kryzinski <adriankryzinski@gmail.com>
2020-03-22 19:31:37 +0100

    Update Main Page sect.

--- src/js/components/Main.js
+++ src/js/components/Main.js
@@ -1,9 +1,29 @@
 /* eslint-disable linebreak-style */
 
+import {select, templates} from '../settings.js';
+import utils from '../utils.js';
+
 class Main {
-  constructor(){
-    // const thisMain = this;
+  constructor(data, id){
+    const thisMain = this;
+
+    thisMain.data = data;
+    thisMain.id = id;
+
+    thisMain.displayMainPage();
+  }
+
+
+  displayMainPage(){
+    const thisMain = this;
+
+    const generatedHTML = templates.mainPage(thisMain.data);
+
+    thisMain.element = utils.createDOMFromHTML(generatedHTML);
+
+    const mainContainer = document.querySelector(select.containerOf.main);
+
+    mainContainer.appendChild(thisMain.element);
   }
 }
-
 export default Main;
