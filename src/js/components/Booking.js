a828faed1246502bf1a0c898279ba92563fade3e
Adrian Kryzinski <adriankryzinski@gmail.com>
2020-03-24 21:00:10 +0100

    Update

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -2,7 +2,7 @@
 
 import { select, templates, settings, classNames } from '../settings.js';
 import utils from '../utils.js';
-import AmountWidget from './amountWidget.js';
+import AmountWidget from './AmountWidget.js';
 import DatePicker from './DatePicker.js';
 import HourPicker from './HourPicker.js';
 
